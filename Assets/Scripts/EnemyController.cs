620d9f01ca943d84901a10b3e59ce32249be7f4c
César Pérez <abog.cesarperez@gmail.com>
2022-01-01 17:35:01 -0300

    EnemyPatrol
    I added several waypoint to the enemy guarding the bridge. Also, mechanics to behave accordingly.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,42 +10,33 @@ public class EnemyController : MonoBehaviour
     private float RotationTime=3f;
     private Animator enemyAnimator;
     private Collider safeHit;
+    private int waypointsIndex;
+    private float distance;
+    private bool sightLock;
     [SerializeField] private Transform eyesTransform;
     [SerializeField] private LayerMask layerToCollide;
     [SerializeField] private float maxDistance;
-    public void Chase()
-    {
-        var distanceVector = target.position - transform.position;
-        var direction = distanceVector.normalized;
+    [SerializeField] private Transform[] waypoints;
+    
 
-        
 
-  
 
-        if (distanceVector.magnitude > MinDistance&&safeHit!=null)
-        {
-            enemyAnimator.SetFloat("Velocity", 1f);
-            
-            transform.position += ChaseSpeed * Time.deltaTime * direction;
-        }
-        else
-        {
-            
-            enemyAnimator.SetFloat("Velocity", 0f);
-        }
-        LookAtPlayer();
-        
-    }
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        waypointsIndex = 0;
+        sightLock = false;
+        transform.LookAt(waypoints[waypointsIndex].position);
         enemyAnimator=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Range();
+        Patrol();
         Chase();
         RayCastEnemyPlayer();
     }
@@ -74,6 +65,78 @@ public class EnemyController : MonoBehaviour
             Debug.Log("No veo al player");
 
         }
+    }
+
+    public void Patrol()
+    {
+        if (sightLock == false)
+        {
+            enemyAnimator.SetFloat("Velocity", 1f);
+            transform.Translate(Vector3.forward * ChaseSpeed * Time.deltaTime);
+        }
+    }
+
+    void IncreaseIndex()
+    {
+        waypointsIndex++;
+        Debug.Log(waypointsIndex);
+        if (waypointsIndex >=waypoints.Length)
+        {
+            waypointsIndex = 0;
+
+        }
+        transform.LookAt(waypoints[waypointsIndex].position);
+    }
+    void Range()
+    {
+        
+        if (sightLock == false) {
+            distance = Vector3.Distance(transform.position, waypoints[waypointsIndex].position);
+            Debug.Log("Incrementando index");
+            if (distance < 1f )
+            {
+                Debug.Log("Incrementando index");
+                IncreaseIndex();
+            }
+        }
+
+    }
+
+    public void Chase()
+    {
+        var distanceVector = target.position - transform.position;
+        var direction = distanceVector.normalized;
+
+
+
+
+
+        if (distanceVector.magnitude > MinDistance && safeHit != null)
+            {
+            sightLock = true;
+                enemyAnimator.SetFloat("Velocity", 1f);
+                enemyAnimator.SetBool("EnemyOnSight", true);
+
+                transform.position += ChaseSpeed * Time.deltaTime * direction;
+                LookAtPlayer();
+            }
+        else if (distanceVector.magnitude <= MinDistance && safeHit != null)
+            {
+
+            enemyAnimator.SetBool("EnemyOnSight", true);
+            enemyAnimator.SetFloat("Velocity", 0f);
+            sightLock = true;
+            LookAtPlayer();
+        }
+
+        else
+        {
+            enemyAnimator.SetBool("EnemyOnSight", false);
+            sightLock = false;
+            transform.LookAt(waypoints[waypointsIndex].position);
+        }
+
+
     }
 
 }
